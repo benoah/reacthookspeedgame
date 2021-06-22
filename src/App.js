ad86e6da7ad2f339f7b9cabadb343565ca804ac2
ben <noahbenaas@gmail.com>
2021-06-22 18:00:34 +0200

    update working with the start button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react"
 
 function App() {
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  
+  function handleChange(e) {
+      const {value} = e.target
+      setText(value)
+  }
+  
+  function calculateWordCount(text) {
+      const wordsArr = text.trim().split(" ")
+      return wordsArr.filter(word => word !== "").length
+  }
+  
+  useEffect(() => {
+      if(isTimeRunning && timeRemaining > 0) {
+          setTimeout(() => {
+              setTimeRemaining(time => time - 1)
+          }, 1000)
+      }
+  }, [timeRemaining, isTimeRunning])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea
+              onChange={handleChange}
+              value={text}
+          />
+          <h4>Time remaining: {timeRemaining}</h4>
+          <button onClick={() => setIsTimeRunning(true)}>Start</button>
+          <h1>Word count: ???</h1>
+      </div>
+  )
 }
 
-export default App;
+export default App
+
+
+/**
+ * Challenge:
+ * 
+ * 1. Create state to hold the current value of the countdown timer.
+ *    Display this time in the "Time Remaining" header
+ * 
+ * 2. Set up an effect that runs every time the `timeRemaining` changes
+ *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
+ * 
+ *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
+ *    a lot of extra work.
+ * 
+ *    Warning: there will be a bug in this, but we'll tackle that next
+ *  
+ *  3. Make it so the effect won't run if the time is already at 0
+
+ */
