2bfd2e8e1a785eda28e4f3984d525b2ff37b0f17
ben <noahbenaas@gmail.com>
2021-06-22 19:21:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
 import React, {useState, useEffect} from "react"
+import './App.css';
+/**
+ * Challenge:
+ * 
+ * When the timer reaches 0, count the number of words the user typed in 
+ * and display it in the "Word count" section
+ * 
+ * After the game ends, make it so the user can click the Start button again
+ * to play a second time
+ */
 
-function App() {
+ function App() {
+  const STARTING_TIME = 5
+  
   const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
   const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
   
   function handleChange(e) {
       const {value} = e.target
@@ -16,13 +28,28 @@ function App() {
       return wordsArr.filter(word => word !== "").length
   }
   
+  function startGame() {
+      setIsTimeRunning(true)
+      setTimeRemaining(STARTING_TIME)
+      setText("")
+  }
+  
+  function endGame() {
+      setIsTimeRunning(false)
+      setWordCount(calculateWordCount(text))
+  }
+  
+  // https://www.google.com/search?q=Disable+button+in+react
+  
   useEffect(() => {
       if(isTimeRunning && timeRemaining > 0) {
           setTimeout(() => {
               setTimeRemaining(time => time - 1)
           }, 1000)
+      } else if(timeRemaining === 0) {
+          endGame()
       }
-  }, [timeRemaining, isTimeRunning])
+  }, [timeRemaining,isTimeRunning])
   
   return (
       <div>
@@ -30,16 +57,21 @@ function App() {
           <textarea
               onChange={handleChange}
               value={text}
+              disabled={!isTimeRunning}
           />
           <h4>Time remaining: {timeRemaining}</h4>
-          <button onClick={() => setIsTimeRunning(true)}>Start</button>
-          <h1>Word count: ???</h1>
+          <button 
+              onClick={startGame}
+              disabled={isTimeRunning}
+          >
+              Start
+          </button>
+          <h1>Word count: {wordCount}</h1>
       </div>
   )
 }
 
 export default App
-
 
 /**
  * Challenge:
